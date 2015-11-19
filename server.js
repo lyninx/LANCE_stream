1bdd56fdc3021fc0a908e7c2f6e1b0f54b7212a9
Dennis Pacewicz <dennis.pacewicz@gmail.com>
2015-11-19 11:01:22 -0500

    added notification posting

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var auth = basicAuth(function(user, pass, callback) {
 app.use(express.static(__dirname + '/public'));
 
 app.get('/admin', auth, function(req, res) {
- res.send('Hello World');
+ res.sendFile(__dirname + '/public/admin.html');
 });
 
 app.get('/stream', function(req, res) {
@@ -26,32 +26,16 @@ app.get('/stream', function(req, res) {
 /////////////////////////////////////////////////////////////////////////
 io.on('connection', function(socket){
 	//console.log('user connected'+socket);
-	socket.join(rooms[0])
-	socket.room = rooms[0];
+	socket.join();
 
 	socket.on('disconnect', function(){
-		//console.log('user disconnected'+socket);
+		console.log('disconnected '+socket);
 	});
 
 	socket.on('chatmessage', function(msg){
-		console.log("|==== ", socket.room)
-		console.log("|== user :", msg.user, msg.id);
-		console.log("|== msg  :", msg.text);
+		console.log(">", msg.time+' '+msg.text);
 		//io.emit('chatmessage', msg);
-		io.sockets.in(socket.room).emit('chatmessage',msg);
-	});
-
-	socket.on('switchRoom', function(newroom){
-		// leave the current room (stored in session)
-		if(!(socket.room == newroom)){X
-			socket.leave(socket.room);
-			//console.log("left "+socket.room);
-			// join new room, received as function parameter
-			socket.join(newroom);
-			//console.log("joined "+newroom);
-			socket.room = newroom;
-		}
-		//socket.emit('updaterooms', rooms, newroom);
+		io.sockets.emit('chatmessage',msg);
 	});
 });
 
